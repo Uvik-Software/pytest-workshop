28d948a08befb7778e23248bd5052db632ca9df1
mykola-uvik <mykola.kokalko@uvik.net>
2019-07-12 19:54:36 +0300

    [Tested] can_land

--- tests/test_airport.py
+++ tests/test_airport.py
@@ -6,3 +6,7 @@ from airport.airport import *
 def test_can_land(get_airship, airport):
     airship = get_airship("Chernivtsi", SMALL)
     assert airport.can_land(airship)
+    assert not airport.can_land(get_airship("Chernivtsi", LARGE))
+    with pytest.raises(Exception) as exc:
+        assert airport.can_land(get_airship("", SMALL))
+    assert str(exc.value) == "Unknown airship, please provide your name if you want to land"
