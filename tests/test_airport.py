f6d2e59242df02cec7b683fe5a29b652c3483d47
mykola-uvik <mykola.kokalko@uvik.net>
2019-07-12 19:45:21 +0300

    [First] factory in fixture

--- tests/test_airport.py
+++ tests/test_airport.py
@@ -5,14 +5,18 @@ from airport.airport import *
 
 @pytest.fixture
 def get_airship():
-    airship = {
-        'name': 'Chernivtsi',
-        'size': SMALL
-    }
-    return airship
+
+    def construct_airship(name, size):
+        airship = {
+            'name': name,
+            'size': size
+        }
+        return airship
+
+    return construct_airship
 
 
 def test_can_land(get_airship):
-    airship = get_airship
+    airship = get_airship("Chernivtsi", SMALL)
     airport = Airport()
     assert airport.can_land(airship)
