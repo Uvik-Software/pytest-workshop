4750225507e2052f7ef66a47a5238cd9caaf1871
mykola-uvik <mykola.kokalko@uvik.net>
2019-07-12 19:43:37 +0300

    Initial test

--- tests/test_airport.py
+++ tests/test_airport.py
@@ -3,5 +3,12 @@ import pytest
 from airport.airport import *
 
 
+
+
 def test_can_land():
-    pass
+    airship = {
+        'name': 'Chernivtsi',
+        'size': SMALL
+    }
+    airport = Airport()
+    assert airport.can_land(airship)
